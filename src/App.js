9ccda02b52d5793f94929d10c8a23622412d5f4c
GUDLA UDAY SAI <udaysaigudla1111@gmail.com>
2022-09-18 21:29:27 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
+import QRCode from 'qrcode';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [url, setUrl] = useState('');
+  const [qrcode, setqrcode] = useState('');
+  const GenerateQRCode = () => {
+      QRCode.toDataURL(url, {
+            width:600,
+            height:600,
+            margin: 2
+      }, (err,url) => {
+        if (err) return console.error(err)
+          console.log(url)
+          setqrcode(url)
+      })
+      
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h1>QRCODE GENERATOR </h1>
+    <div className='flex-container'>
+      <div className='flex-box'>
+    <textarea rows="5" cols="60" 
+   className='textarea'
+    placeholder="Write any http or content or whatever ............."
+    value={url}
+    onChange={(evt) => setUrl(evt.target.value)}
+    />
+    <br></br>
+    <button className="btn" onClick={GenerateQRCode}>Generate</button>
+    </div>
+    <div className='flex-box'>
+    {qrcode && <>
+      <img src={qrcode}  className="img" />
+      <a href={qrcode} className="btn1" download="qrcode.png">Download</a>
+    </>}
+    </div>
+    </div>
     </div>
   );
 }
